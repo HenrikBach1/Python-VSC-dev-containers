2516ee1e9ecaef4ae841296fe3f416799b28cc08
Henrik Bach <hbac@kea.dk>
2021-04-04 12:35:07 +0000

    Added reference

--- python/Network/Server.py
+++ python/Network/Server.py
@@ -5,6 +5,7 @@
 #     Testing the code [with PuTTY]: https://techtutorialsx.com/2018/06/02/esp8266-arduino-socket-server/
 #     How to Expose Ports in Docker: https://www.whitesourcesoftware.com/free-developer-tools/blog/docker-expose-port/
 #     Forwarding or publishing a port [in Docker]: https://code.visualstudio.com/docs/remote/containers#_forwarding-or-publishing-a-port
+#     VS Code with Python in Docker - Quickstart: https://dev.to/siaarzh/vs-code-with-python-in-docker-quickstart-3ph4#:~:text=Say%20you%20started%20your%20server%20with%20python%20manage.py,%22port%22%20and%20select%20%22Remote-Containers%3A%20Forward%20Port%20from%20Container...%22.
 #     Remote debugging Python with VSCode: https://www.benoitpatra.com/2017/11/27/remote-debugging-python-with-vscode/
 import socket                                         
 
