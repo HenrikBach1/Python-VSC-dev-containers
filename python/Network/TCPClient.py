85f90769cfcc42cf715e7515b6b6e6944ec15072
Henrik Bach <hbac@kea.dk>
2021-04-10 13:00:27 +0000

    Added TCP example files

---
+++ python/Network/TCPClient.py
@@ -0,0 +1,41 @@
+# Inspired from
+#     https://python-forum.io/Thread-BrokenPipeError-Errno-32-Broken-pipe
+
+# Globals
+port = 9999
+
+# Echo client program
+import socket
+import sys
+ 
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+# Get local machine name
+host = socket.gethostname()
+server_address = (host, port)
+
+print("Connecting to '{}'".format(server_address))
+sock.connect(server_address)
+ 
+try:
+    # Send data
+    message = ('This is the message.  It will be repeated.')
+    enc_mes = message.encode('utf-8')
+    print("Sending '{}'".format(message))
+    sock.sendall(enc_mes)
+ 
+    # Look for the response
+    amount_received = 0
+    amount_expected = len(message)
+ 
+    while amount_received < amount_expected:
+        chunk = 1024
+        data = sock.recv(chunk)
+        amount_received += len(data)
+        dec_data = data.decode('utf-8')
+        print("Received {}".format(dec_data))
+ 
+finally:
+    sock.close()
+    print('Socket closed')
---
+++ python/Network/TCPServer.py
@@ -0,0 +1,46 @@
+# Inspired from
+#     https://python-forum.io/Thread-BrokenPipeError-Errno-32-Broken-pipe
+
+# Globals
+port = 9999
+
+# Echo server program
+import socket
+import sys
+ 
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+# Get local machine name
+host = socket.gethostname()
+server_address = (host, port)
+
+# Bind the socket to the port
+print ("Listening on '{}'...".format(server_address))
+sock.bind(server_address)
+sock.listen(1)
+ 
+while True:
+    print('Waiting for a client connection...')
+    (clientSocket, client_address) = sock.accept()
+ 
+    try:
+        print ("Connected to client: '{}'".format(client_address))
+ 
+        # Receive the data in small chunks and retransmit it
+        while True:
+            chunk = 1024
+            data = clientSocket.recv(chunk)
+            dec_data = data.decode('utf-8')
+            print("Data received: '{}'".format(dec_data))
+            if dec_data:
+                print('Echoing data to the client...')
+                clientSocket.sendall(data)
+            else:
+                print("No more data from client: '{}'.".format(client_address))
+                break
+ 
+    finally:
+        # Clean up the connection
+        clientSocket.close()
+        print('Client socket closed.')
